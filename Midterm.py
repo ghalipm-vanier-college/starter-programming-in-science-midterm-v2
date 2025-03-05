617ec803f1a1c5092a30a9cf6b75cbec8a426a27
Ghalip Memet <45562923+ghalipm@users.noreply.github.com>
2025-03-05 02:55:47 -0500

    Create Midterm.py
    first commit - need update

---
+++ Midterm.py
@@ -0,0 +1,42 @@
+import math
+
+# Q1: Calculate the area of an ellipse
+def area_of_ellipse(a, b):
+    return 0.0
+
+# Q2: Find the sum of numbers
+def sum_of_numbers(numbers):
+    return 0
+
+# Q3: Upper left triangle
+def upper_left_triangle(n):
+    return ""
+
+# Q4: Hollow parallelogram with stars - left leaning
+def hollow_left_parallelogram(n):
+    return ""
+
+# ----------------------------------------------------------------
+print(sum_of_numbers([5, 2, 8, 10]))
+print()
+
+print(area_of_ellipse(5, 6))
+print()
+
+print(upper_left_triangle(2))
+print()
+
+print(upper_left_triangle(3))
+print()
+
+print(upper_left_triangle(5))
+print()
+
+print(hollow_left_parallelogram(2))
+print()
+
+print(hollow_left_parallelogram(3))
+print()
+
+print(hollow_left_parallelogram(4))
+print()
