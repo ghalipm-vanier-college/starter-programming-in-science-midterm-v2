e75c5406d5c16ffd86ec4afcf604fb1b0dad9ecc
Ghalip Memet <45562923+ghalipm@users.noreply.github.com>
2025-03-05 10:48:46 -0500

    Update test_midterm.py
    test file first update

--- test_midterm.py
+++ test_midterm.py
@@ -1,30 +1,30 @@
-from Midterm import area_of_ellipse, sum_of_numbers, upper_left_triangle, hollow_left_parallelogram
+from Midterm import area_of_parallelogram, sum_upto, lower_left_triangle, hollow_right_parallelogram
 
-# Test Q1: area_of_ellipse
-def test_area_of_ellipse():
-    assert area_of_ellipse(5,6) == 94.25
-    assert area_of_ellipse(10,10) == 314.16
-    assert area_of_ellipse(0,10) == 0.0
-    assert area_of_ellipse(2,5) == 31.42
+# Test Q1: area_of_parallelogram
+def test_area_of_parallelogram():
+    assert area_of_parallelogram(5,6,30) == 15.00
+    assert area_of_parallelogram(10,10, 60) == 86.60
+    assert area_of_parallelogram(0,10, 45) == 0.0
+    assert area_of_parallelogram(4,5, 90) == 20.00
 
 # Test Q2: sum_of_numbers
 def test_sum_of_numbers():
-    assert sum_of_numbers([2,8,10,5])== 25
-    assert sum_of_numbers([2,-7,10,8,9]) == 22
-    assert sum_of_numbers([-9,4,5]) == 0.0
-    assert sum_of_numbers([-2,-5, -7, -6]) == -20
+    assert sum_of_numbers(100)== 5050
+    assert sum_of_numbers(10) == 55
+    assert sum_of_numbers(0) == 0
     
-# Test Q3: upper_left_triangle
-def test_upper_left_triangle():
-    assert upper_left_triangle(2) == "The triangle height should be at least 3."
-    assert upper_left_triangle(4) == "* * * * \n* * * \n* * \n*"
-    assert upper_left_triangle(5) == "* * * * * \n* * * * \n* * * \n* * \n*"
+# Test Q3: lower_left_triangle
+def test_lower_left_triangle():
+    assert lower_left_triangle(2) == "The triangle height should be at least 3."
+    assert lower_left_triangle(4) == "* \n* * \n* * * \n* * * *"
+    assert lower_left_triangle(5) == "* \n* * \n* * * \n* * * * \n* * * * *"
 
 # Test Q4: hollow_left_parallelogram
 def test_hollow_left_parallelogram():
     assert hollow_left_parallelogram(2) == "The rows should be at least 3."
-    assert hollow_left_parallelogram(3) == "* * * \n *   * \n  * * *"
-    assert hollow_left_parallelogram(5) == "* * * * * \n *       * \n  *       * \n   *       * \n    * * * * *"
+    assert hollow_left_parallelogram(3) == "  * * * \n *   * \n* * *"
+    assert hollow_left_parallelogram(4) == "   * * * * \n  *     * \n *     * \n* * * *"
+    assert hollow_left_parallelogram(5) == "    * * * * * \n   *       * \n  *       * \n *       * \n* * * * *"
 
 
 # Run tests
