c229171e241f4c4051b258209620f34a6a6b9963
Ghalip Memet <45562923+ghalipm@users.noreply.github.com>
2025-03-05 23:27:20 -0500

    Update Midterm.py
    mix up corrected

--- Midterm.py
+++ Midterm.py
@@ -1,19 +1,19 @@
 import math
 
 # Q1: Calculate the area of an parallelogram
-def (a,b, \theta):
+def area_of_parallelogram(a,b, \theta):
     return 0.0
 
 # Q2: Find the sum of numbers from 1 to n (including n)
 def sum_upto(n):
     return 0
 
-# Q3: Upper left triangle
-def upper_left_triangle(n):
+# Q3: Lower left triangle
+def lower_left_triangle(n):
     return ""
 
 # Q4: Hollow parallelogram with stars - left leaning
-def hollow_left_parallelogram(n):
+def hollow_right_parallelogram(n):
     return ""
 
 # ----------------------------------------------------------------
@@ -23,20 +23,20 @@ print()
 print(area_of_parallelogram(5, 6, 30))
 print()
 
-print(upper_left_triangle(2))
+print(lower_left_triangle(2))
 print()
 
-print(upper_left_triangle(3))
+print(lower_left_triangle(3))
 print()
 
-print(upper_left_triangle(5))
+print(lower_left_triangle(5))
 print()
 
-print(hollow_left_parallelogram(2))
+print(hollow_right_parallelogram(2))
 print()
 
-print(hollow_left_parallelogram(3))
+print(hollow_right_parallelogram(3))
 print()
 
-print(hollow_left_parallelogram(4))
+print(hollow_right_parallelogram(4))
 print()
