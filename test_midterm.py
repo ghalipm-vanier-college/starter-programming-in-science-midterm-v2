3930a89ab236dd37b47db2565bb6b4c38e166559
Ghalip Memet <45562923+ghalipm@users.noreply.github.com>
2025-03-05 23:34:25 -0500

    Update test_midterm.py
    corrected mix up

--- test_midterm.py
+++ test_midterm.py
@@ -9,12 +9,12 @@ def test_area_of_parallelogram():
     assert area_of_parallelogram(4,5, 90) == 20.00
 
 # Test Q2: sum_of_numbers
-def test_sum_of_numbers():
-    assert sum_of_numbers(100)== 5050
-    assert sum_of_numbers(10) == 55
-    assert sum_of_numbers(0) == 0
+def test_sum_upto():
+    assert sum_upto(100)== 5050
+    assert sum_upto(10) == 55
+    assert sum_upto(0) == 0
     
-# Test Q3: lower_left_triangle
+# Test Q3: lower left triangle
 def test_lower_left_triangle():
     assert lower_left_triangle(2) == "The triangle height should be at least 3."
     assert lower_left_triangle(4) == "* \n* * \n* * * \n* * * *"
@@ -22,10 +22,10 @@ def test_lower_left_triangle():
 
 # Test Q4: hollow_left_parallelogram
 def test_hollow_left_parallelogram():
-    assert hollow_left_parallelogram(2) == "The rows should be at least 3."
-    assert hollow_left_parallelogram(3) == "  * * * \n *   * \n* * *"
-    assert hollow_left_parallelogram(4) == "   * * * * \n  *     * \n *     * \n* * * *"
-    assert hollow_left_parallelogram(5) == "    * * * * * \n   *       * \n  *       * \n *       * \n* * * * *"
+    assert hollow_right_parallelogram(2) == "The rows should be at least 3."
+    assert hollow_right_parallelogram(3) == "  * * * \n *   * \n* * *"
+    assert hollow_right_parallelogram(4) == "   * * * * \n  *     * \n *     * \n* * * *"
+    assert hollow_right_parallelogram(5) == "    * * * * * \n   *       * \n  *       * \n *       * \n* * * * *"
 
 
 # Run tests
